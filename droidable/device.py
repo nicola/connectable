bb1376ab97d2ab54f9457d89911bd5d4c21bce45
Nicola Greco <notsecurity@gmail.com>
2013-10-31 02:27:23 +0000

    dictionary or name and path for Device

--- droidable/device.py
+++ droidable/device.py
@@ -1,9 +1,16 @@
 from droidable.hook import EventHook
 
 class Device(object):
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
+    def __init__(self, dictionary, path = None):
+        if (path is not None):
+            self.device = self.name = dictionary;
+            self.path = path
+            
+        if ('name'    in dictionary):     self.name = dictionary['name']
+        if ('mac'     in dictionary):      self.mac = dictionary['mac']
+        if ('type'    in dictionary):     self.type = dictionary['type']
+        if ('triggers'in dictionary): self.triggers = dictionary['triggers']
+        if ('actions' in dictionary):  self.actions = dictionary['actions']
         
 class DeviceCollection(object):
     onAdded = EventHook()
