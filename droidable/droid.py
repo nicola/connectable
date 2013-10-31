716b7e0099dbe0496cc4d7c2595897bd8f0f431d
Nicola Greco <notsecurity@gmail.com>
2013-10-31 02:28:21 +0000

    dictionary or name and path for Device

--- droidable/droid.py
+++ droidable/droid.py
@@ -3,10 +3,13 @@ from droidable.device import DeviceCollection
 
 class Droid:
     """Creates a droid"""
-    devices = DeviceCollection()
-
-    def run(self):
-        
+    _devices = DeviceCollection()
     
+    def plug(self):
+        pass
+    
+    def unplug(self):
+        pass
+        
     def __init__(self, name):
         self.name = name
