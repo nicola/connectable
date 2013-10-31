9fc0898588e10d058e1c2763fa8277e717cf6f69
Nicola Greco <notsecurity@gmail.com>
2013-10-31 13:03:50 +0000

    adding scripts

--- droidable/droid.py
+++ droidable/droid.py
@@ -6,19 +6,25 @@ class Droid:
     """Creates a droid"""
     _devices = Collection()
     _scripts = Collection()
+    _conditions_tree = {}
+    _conditions_hashes = {}
     
     # Grouped by kind
     _triggers = {}
     _actions = {}
         
     def __init__(self, name = ""):
-        pass
+        self.generate_conditions()
         
     def add_device (self, device):
         _devices.add(device)
 
     def add_script (self, script):
         _scripts.add(script)
+        
+    def generate_conditions(self):
+        for script in _script.collection():
+            print script.conditions_hash
         
     def run():
         print "Droid running"
