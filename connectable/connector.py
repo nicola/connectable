2ded77656e90f30521449af945b61a4cdcd0e506
Nicola Greco <notsecurity@gmail.com>
2013-11-03 10:05:49 +0000

    new compiler class

--- connectable/connector.py
+++ connectable/connector.py
@@ -4,12 +4,14 @@ from connectable.device import Device
 from connectable.device import Trigger
 from connectable.device import Action
 from connectable.script import Script
+from connectable.compiler import Compiler
 
 class Connector:
     """A `connector` connects through `script`s `connectable`'s triggers and actions"""
         
     def __init__(self, name = ""):
         """Constructor"""
+        self._compiler = Compiler(self)
         self._devices = {}
         self._scripts = {}
     
@@ -32,55 +34,14 @@ class Connector:
             else:
                 return False
 
-    def link_from_object(self, prop):
-        """Linking function: From a device object return the one in _devices"""
-        # TODO check if property exists
-        if (prop.device.kind in self._devices and prop.device.ID in self._devices[prop.device.kind]):
-            return self._devices[prop.device.kind][prop.device.ID]
-        else:
-            return False
-
-    def link_from_string(self, prop_name, prop):
-        """Linking function: From an id or kind return the closest corresponding one in _devices"""
-        device = self.device_with_ID(prop.device)
-        if (device):
-            if (isinstance(prop, Trigger) and prop_name in device.triggers): return device
-            if (isinstance(prop, Action) and prop_name in device.actions): return device
-        
-        if (prop.device in self._devices):
-            # check in family
-            for device in self._devices[prop.device]:
-                if (isinstance(prop, Trigger) and prop_name in device.triggers): return device
-                if (isinstance(prop, Action) and prop_name in device.actions): return device
-                
-        # TODO error raised to be improved
-        raise
-
-    def link_connectable(self, prop_name, prop):
-        """Linking function: returns a device in _devices given the reference in a Script"""
-        if (isinstance(prop.device, Device)):
-            return self.link_from_object(prop)
-            
-        # TODO what about dictionaries?
-
-        return self.link_from_string(prop_name, prop)
-            
-    def link_connectables(self, script):
-        """Linking functions: connects connectable to the script"""
-        for trigger_name, trigger in script.triggers.iteritems():
-            trigger.device = self.link_connectable(trigger_name, trigger)
-            
-        for action_name, action in script.triggers.iteritems():
-            action.device = self.link_connectable(action_name, action)
-        
-
     def add_script (self, script):
         """Adds a script"""
 
-        self.link_connectables(script)
+        self._compiler.link_connectables(script)
         self._scripts[script.ID] = script
         self.generate_conditions()
-        
+    
+    # DEPRECATED
     def generate_conditions_by_family(self):
         
         conditions_tree = {}
